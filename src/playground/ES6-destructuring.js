ef8f6259c05a9019fef8f9feddac372b9066ad34
cptiwari20 <cptiwari20@gmail.com>
2018-12-01 10:29:00 +0530

    understanding the JS ES6 Array desructuring

--- src/playground/ES6-destructuring.js
+++ src/playground/ES6-destructuring.js
@@ -1,15 +1,15 @@
-const students = {
-  totalNumber: 200,
-  passed: 160,
-  failed: 20,
-  ATKT: {
-    chemistry: 1,
-    physics: 2,
-    botany: 3,
-    zoology: 0,
-    maths: 14
-  }
-};
+// const students = {
+//   totalNumber: 200,
+//   passed: 160,
+//   failed: 20,
+//   ATKT: {
+//     chemistry: 1,
+//     physics: 2,
+//     botany: 3,
+//     zoology: 0,
+//     maths: 14
+//   }
+// };
 
 // const {failed} = students;
 // console.log(failed);
@@ -19,6 +19,21 @@ const students = {
 // console.log(zoology);
 
 // students.ATKT.NewSubject = 11;
-const { NewSubject: value = 10 } = students.ATKT;
-// console.log(students.ATKT)
-console.log(value)
+// const { NewSubject: value = 10 } = students.ATKT;
+// // console.log(students.ATKT)
+// console.log(value)
+
+
+// ES 6 Destructuring the Arrays
+
+const place = ['Kanour', 'Barhi', 'Katni', '483770'];
+
+// const [, tehshil, district, PIN] = place; // leaving the 0th index sting
+// const [, tehshil, , PIN] = place; // leaving the name of sting at index of 2
+const [,,, tehshil] = place; //naming the last sting only; 
+
+
+// console.log('My area pin is', PIN);
+// console.log('My Village name is', place[0]);
+// console.log(district, 'is my district');
+console.log(tehshil);
