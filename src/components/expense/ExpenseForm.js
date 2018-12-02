52be9a240871e1b8c90844aca176f9227da5bd1a
cptiwari20 <cptiwari20@gmail.com>
2018-12-02 21:27:49 +0530

    integrated the react-dates singledatepicker

--- src/components/expense/ExpenseForm.js
+++ src/components/expense/ExpenseForm.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
+console.log(moment())
 class ExpenseForm extends Component {
+  state = {
+    description: '',
+    amount: '',
+    note: '',
+    createdAt: moment(),
+    calenderFocused: false
+  }
+  onDateChange = ({ createdAt }) => (
+    this.setState({ createdAt })
+  )
+  onFocusChange = ({ focused }) => {
+    this.setState({ calenderFocused: focused})
+  }
   render(){
     return (
-      <h1>
-      ExpenseForm
-      </h1>
+      <form>
+        <input 
+          type='text'
+          placeholder='Description'  
+        />
+        <SingleDatePicker 
+          date={this.state.createdAt}
+          onDateChange={this.onDateChange}
+          focused={this.state.calenderFocused}
+          onFocusChange={this.onFocusChange}
+        />
+      </form>
     )
   }
 }
