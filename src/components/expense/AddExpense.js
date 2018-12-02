d908d1fc264e57c05f87dedea0ae389159c6a9b0
cptiwari20 <cptiwari20@gmail.com>
2018-12-02 20:46:48 +0530

    added expense Form file

--- src/components/expense/AddExpense.js
+++ src/components/expense/AddExpense.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import ExpenseForm from './ExpenseForm';
 
 class AddExpense extends Component{
   render(){
     return(
       <div>
-        <h2>AddExpense Add Your new expense..</h2>
-      </div>
+        <h2>Add Your New Expense.</h2>
+        <ExpenseForm />
+        </div>
     )
   }
 }
---
+++ src/components/expense/ExpenseForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ExpenseForm extends Component {
+  render(){
+    return (
+      <h1>
+      ExpenseForm
+      </h1>
+    )
+  }
+}
+
+export default ExpenseForm;
