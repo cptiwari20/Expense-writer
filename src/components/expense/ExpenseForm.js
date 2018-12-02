8ad1d96a817ffe48e9c16baf6f22e6ffc909228f
cptiwari20 <cptiwari20@gmail.com>
2018-12-02 21:50:03 +0530

    added some reg expressions for hadle the amount

--- src/components/expense/ExpenseForm.js
+++ src/components/expense/ExpenseForm.js
@@ -8,30 +8,52 @@ console.log(moment())
 class ExpenseForm extends Component {
   state = {
     description: '',
-    amount: '',
+    amount: 0,
     note: '',
     createdAt: moment(),
     calenderFocused: false
-  }
+  } 
   onDateChange = ({ createdAt }) => (
     this.setState({ createdAt })
   )
   onFocusChange = ({ focused }) => {
     this.setState({ calenderFocused: focused})
   }
+  onAmountChange = e => {
+    const amount = e.target.value;
+    if(amount.match(/^\d*(\.\d{0,2})?$/)){
+     return this.setState( { amount })
+    }
+  }
   render(){
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input 
           type='text'
-          placeholder='Description'  
+          placeholder='Description'
+          value={this.state.description}
+          onChange={e => this.setState({description: e.target.value})}  
+        />
+        <input 
+          type='number'
+          placeholder='Amount'
+          value={this.state.amount}
+          onChange={this.onAmountChange}  
         />
         <SingleDatePicker 
           date={this.state.createdAt}
           onDateChange={this.onDateChange}
           focused={this.state.calenderFocused}
           onFocusChange={this.onFocusChange}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
         />
+        <textarea 
+          placeholder='Note'
+          value={this.state.note}
+          onChange={e => this.setState({note: e.target.value})}  
+        ></textarea>
+        <button type='submit'>Submit</button>
       </form>
     )
   }
