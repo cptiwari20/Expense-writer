335abf68d5e94293e23deb4de83528632f430195
cptiwari20 <cptiwari20@gmail.com>
2018-12-07 20:43:46 +0530

    added redux dev tools for chrome

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,6 +6,8 @@ export default () => {
   const store = createStore(combineReducers({
     expenses: expenseReducer,
     filters: filterReducer 
-  }));
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
   return store;
 };
