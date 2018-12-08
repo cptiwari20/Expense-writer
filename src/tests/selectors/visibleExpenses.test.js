4cf129d1c1823d45dbb65065e5ef4cd06c09f3dd
cptiwari20 <cptiwari20@gmail.com>
2018-12-08 17:42:10 +0530

    added test for expenses selectors

--- src/tests/selectors/visibleExpenses.test.js
+++ src/tests/selectors/visibleExpenses.test.js
@@ -31,6 +31,51 @@ describe('VISIBLE EXPENSES', () => {
       endDate: undefined
     };
     const visibleExpense = visibleExpenses(expenses, filters); 
-    expect(visibleExpense).toEqual([ expenses[1]])
-  })
+    expect(visibleExpense).toEqual([ expenses[1] ])
+  });
+
+  it('should show the expenses sort by start Date', () => {
+    const filters = {
+      text: '',
+      sortBy: 'date',
+      startDate: moment(),
+      endDate: undefined
+    }; 
+    const visibleExpense = visibleExpenses(expenses, filters); 
+    expect(visibleExpense).toEqual([ expenses[0] ])
+  });
+
+  it('should show the expenses sort by end date', () => {
+    const filters = {
+      text: '',
+      sortBy: 'date',
+      startDate: undefined,
+      endDate: moment()
+    }; 
+    const visibleExpense = visibleExpenses(expenses, filters); 
+    expect(visibleExpense).toEqual([ expenses[1], expenses[2] ])
+  });
+ 
+  it('should show the expenses sort by date', () => {
+    const filters = {
+      text: '',
+      sortBy: 'date',
+      startDate: undefined,
+      endDate: undefined
+    };
+    const visibleExpense = visibleExpenses(expenses, filters); 
+    expect(visibleExpense).toEqual([ expenses[1], expenses[2], expenses[0] ])
+  });
+
+  it('should show the expenses sort by amount', () => {
+    const filters = {
+      text: '',
+      sortBy: 'amount',
+      startDate: undefined,
+      endDate: undefined
+    }; 
+    const visibleExpense = visibleExpenses(expenses, filters); 
+    expect(visibleExpense).toEqual([  expenses[2], expenses[0], expenses[1] ])
+  });
+  
 }) 
