192ab7b0b0c01d079e7ea69b6ff23f3ca77af39a
cptiwari20 <cptiwari20@gmail.com>
2018-12-08 18:05:33 +0530

    adding test after refactor

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -4,37 +4,45 @@ import moment from 'moment';
 const defaultState = {
   text: '',
   sortBy: '',
-  startDate: moment().startOf('month'),
-  endDate: moment().endOf('month')
+  startDate: undefined,
+  endDate: undefined
 }
 
 describe('Filters Reducers', () => {
   it('should add text filter', () => {
     const action = {type: 'FILTER_BY_TEXT', text: 'rom'};
     const newState = filterReducer(defaultState, action);
-    expect(newState).toEqual({
-      ...defaultState,
-      text: 'rom'
-    })
+    expect(newState.text).toBe('rom')
   });
 
   it('should add sort by date', () => {
     const action = {type: 'SORT_BY_DATE'};
     const newState = filterReducer(defaultState, action);
-    expect(newState).toEqual({
-      ...defaultState,
-      sortBy: 'date'
-    })
+    expect(newState.sortBy).toBe('date')
   });
 
   it('should add sort by amount', () => {
     const action = {type: 'SORT_BY_AMOUNT'};
     const newState = filterReducer(defaultState, action);
-    expect(newState).toEqual({
-      ...defaultState,
-      sortBy: 'amount'
-    })
+    expect(newState.sortBy).toBe('amount')
   });
 
+  it('should add set by start date', () => {
+    const action = {type: 'SET_START_DATE', startDate: 123};
+    const newState = filterReducer(defaultState, action);
+    expect(newState).toEqual({
+      ...defaultState,
+      startDate: 123
+    });
+  });
+
+  it('should add set by end date', () => {
+    const action = {type: 'SET_END_DATE', endDate: 123};
+    const newState = filterReducer(defaultState, action);
+    expect(newState).toEqual({
+      ...defaultState,
+      endDate: 123
+    })
+  });
 
 })
