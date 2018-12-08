e13aa2aebd64d677b6bf3fce555c0e6ae1e9751c
cptiwari20 <cptiwari20@gmail.com>
2018-12-08 16:04:22 +0530

    added tests for filters action

--- src/tests/actions/filter.test.js
+++ src/tests/actions/filter.test.js
@@ -2,7 +2,7 @@ import {filterByText, setEndDate, setStartDate, sortByAmount, sortByDate} from '
 
 
 describe('Filter ACTIONS', () => {
-  it('should create an action for FILTER_BY_TExT', () => {
+  it('should return action with type "FILTER_BY_TExT" ', () => {
     const text = 'mm';
     const action = filterByText(text);
     expect(action).toEqual({
@@ -10,7 +10,7 @@ describe('Filter ACTIONS', () => {
       text
     })
   });
-  it('should create an action without ant text', () => {
+  it('should return action without any text', () => {
     const action = filterByText();
     expect(action).toEqual({
       type: 'FILTER_BY_TEXT',
@@ -18,17 +18,33 @@ describe('Filter ACTIONS', () => {
     })
   })
 
-  it('should return an action for the "SORT_BY_DATE"', () => {
+  it('should return an action with type "SORT_BY_DATE"', () => {
     const action = sortByDate();
     expect(action).toEqual({
       type: 'SORT_BY_DATE'
     });
   })
 
-  it('should return an action for the "SORT_BY_AMOUNT"', () => {
+  it('should return an action with type "SORT_BY_AMOUNT"', () => {
     const action = sortByAmount();
     expect(action).toEqual({
       type: 'SORT_BY_AMOUNT'
+    });
+  })
+
+  it('should return an action with type "SET_START_DATE"', () => {
+    const fromDate = 15462
+    const action = setStartDate(fromDate);
+    expect(action).toEqual({
+      type: 'SET_START_DATE', startDate: fromDate
+    });
+  })
+
+  it('should return an action with type "SET_END_DATE"', () => {
+    const toDate = 15462
+    const action = setEndDate(toDate);
+    expect(action).toEqual({
+      type: 'SET_END_DATE', endDate: toDate
     });
   })
 
