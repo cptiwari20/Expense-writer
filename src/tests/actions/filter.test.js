2e8edc4b3bfabf1b5467dbabe6772e94f22c479f
cptiwari20 <cptiwari20@gmail.com>
2018-12-08 15:58:49 +0530

    test for SortByAmount and sortByDate

--- src/tests/actions/filter.test.js
+++ src/tests/actions/filter.test.js
@@ -18,4 +18,18 @@ describe('Filter ACTIONS', () => {
     })
   })
 
+  it('should return an action for the "SORT_BY_DATE"', () => {
+    const action = sortByDate();
+    expect(action).toEqual({
+      type: 'SORT_BY_DATE'
+    });
+  })
+
+  it('should return an action for the "SORT_BY_AMOUNT"', () => {
+    const action = sortByAmount();
+    expect(action).toEqual({
+      type: 'SORT_BY_AMOUNT'
+    });
+  })
+
 })
