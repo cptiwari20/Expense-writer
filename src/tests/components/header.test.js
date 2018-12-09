616bf08975f564627cef615caaabe400c928eb3d
cptiwari20 <cptiwari20@gmail.com>
2018-12-09 12:28:21 +0530

    added snapshot testing with adding some automate serializers

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { shallow } from 'enzyme';
-import  toJson from "enzyme-to-json";
+// import  toJson from "enzyme-to-json";
 import Header from "../../components/Header";
 
 it('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect(toJson(wrapper)).toMatchSnapshot()
+  expect(wrapper).toMatchSnapshot()
 })
 
 // import ReactShallowRenderer from "react-test-renderer/shallow";
