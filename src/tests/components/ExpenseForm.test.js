b57adaa5d09518f43a9e6c0cf542caa3b1632e5f
cptiwari20 <cptiwari20@gmail.com>
2018-12-09 19:23:54 +0530

    added test for testing the error

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -3,13 +3,30 @@ import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/expense/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
-it('should correctly render ExpenseForm without any value', () => {
-  const wrapper = shallow(<ExpenseForm />)
-  expect(wrapper).toMatchSnapshot();
-})
+describe('Expense Form', () => {  
+  it('should correctly render ExpenseForm without any value', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    expect(wrapper).toMatchSnapshot();
+  });
 
-it('should correctly render ExpenseForm with some value', () => {
-  const wrapper = shallow(<ExpenseForm expense={ expenses[1] } />)
-  expect(wrapper).toMatchSnapshot();
+  it('should correctly render ExpenseForm with some value', () => {
+    const wrapper = shallow(<ExpenseForm expense={ expenses[1] } />)
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('Expense Form Handlers', () => {
+  let wrapper;
+  beforeEach(() => wrapper = shallow(<ExpenseForm />))
+
+  it('should show an error while submitting the empty form', () => {
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('form').simulate('submit', {
+      preventDefault: () => {}
+    });
+
+    expect(wrapper.state('error').length).toBeGreaterThan(0)
+    expect(wrapper).toMatchSnapshot();
+  })
 })
 
