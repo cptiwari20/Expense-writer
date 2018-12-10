13a312970b84b7116e3090c173a749fe7dba1e6c
cptiwari20 <cptiwari20@gmail.com>
2018-12-10 10:47:13 +0530

    defined mode inside webpack using CLI scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,38 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: ["@babel/plugin-proposal-class-properties"]
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
         }
-      }
-    },{
-      test: /\.s?css$/,
-      use: [
-        'style-loader', // templates CSS into normal JS
-        'css-loader', // creates style nodes from JS strings 
-        'sass-loader' // compiles the Sass to CSS by using the node-sass 
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    port: 5000
+      },{
+        test: /\.s?css$/,
+        use: [
+          'style-loader', // templates CSS into normal JS
+          'css-loader', // creates style nodes from JS strings 
+          'sass-loader' // compiles the Sass to CSS by using the node-sass 
+        ]
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      port: 5000
+    }
   }
-}
+};
