c3374ebacf2f35a81f31fb46731ecff28dc6c69a
cptiwari20 <cptiwari20@gmail.com>
2018-12-19 20:45:00 +0530

    cleanup the test code

--- src/app.js
+++ src/app.js
@@ -8,40 +8,6 @@ import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
 const store = configureStore();
-import firebase from 'firebase'
-const db = firebase.database();
-
-
-// db.ref().set({
-//   name: 'Cp Tiwari',
-//   age: '24',
-//   location: {
-//     city: 'Jbp',
-//     state: 'MP'
-//   }
-// });
-
-// db.ref('location/state').set('Madhya Pradesh')
-// db.ref().update({ name: 'Vikas Tiwari', "location/city": "Katni"}).then(() => {
-//   console.log('Data Updates');
-// }).catch(e => console.log(e));
-
-const onVC = db.ref().on('value', (snapshot) => {
-  let val = snapshot.val();
-  console.log(val);
-});
-
-// setTimeout(()=> {
-//   db.ref('age').set('22');
-// }, 10000);
-db.ref().off(onVC);
-
-
-
-setTimeout(()=> {
-  db.ref().update({'location/city': 'Barhi Madhya Pradesh'})
-}, 11000)
-
 ReactDOM.render(
   <Provider store={store}>
     <AppRoutes />
