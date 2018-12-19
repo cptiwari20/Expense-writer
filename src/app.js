fdf47ca66b6e41ff17dfe201e8617c6e18b09abe
cptiwari20 <cptiwari20@gmail.com>
2018-12-19 21:16:41 +0530

    firebase push new table

--- src/app.js
+++ src/app.js
@@ -8,6 +8,20 @@ import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
 const store = configureStore();
+import firebase from 'firebase'
+const db = firebase.database();
+
+const expenses =
+{
+  description: 'Laptop',
+  amount: '3000000',
+  createdAt: '12535434561',
+  note: 'This is an expense'
+}
+
+db.ref('expenses').push(expenses)
+
+
 ReactDOM.render(
   <Provider store={store}>
     <AppRoutes />
