73e614aabb2cbdaedceab99814e975311831d30d
cptiwari20 <cptiwari20@gmail.com>
2018-12-19 21:50:05 +0530

    setup firebase

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRoutes from './router/AppRoutes';
 import configureStore from './store/configureStore';
+
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
-const store = configureStore();
 
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
