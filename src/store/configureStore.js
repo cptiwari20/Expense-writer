56abad93ba4c0e3e29fe4d05b7e144eb44cf6a78
cptiwari20 <cptiwari20@gmail.com>
2018-12-20 12:44:19 +0530

    added redux-thunk middleware to the store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,17 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import ReduxThunk from "redux-thunk";
 import expenseReducer from '../reducers/expenses';
 import filterReducer from '../reducers/filters';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default () => {
-  const store = createStore(combineReducers({
-    expenses: expenseReducer,
-    filters: filterReducer 
-  }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  const store = createStore(
+    combineReducers ({
+      expenses: expenseReducer,
+      filters: filterReducer 
+    }), 
+    composeEnhancers(applyMiddleware(ReduxThunk))
   );
   return store;
 };
