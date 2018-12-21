8421e7777e7f374ef1c302932d7c48c8370eb891
cptiwari20 <cptiwari20@gmail.com>
2018-12-21 15:38:59 +0530

    added dotenv module

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,4 +1,6 @@
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Dotenv from 'dotenv';
 
 enzyme.configure({ adapter: new Adapter() });
+Dotenv.configure({ env: '.env.test'})
