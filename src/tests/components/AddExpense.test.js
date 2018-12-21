5fe400c62008d6747488df5734d9591d3fdae9e3
cptiwari20 <cptiwari20@gmail.com>
2018-12-21 13:39:47 +0530

    retest the addExpense component

--- src/tests/components/AddExpense.test.js
+++ src/tests/components/AddExpense.test.js
@@ -4,11 +4,11 @@ import { AddExpense }from '../../components/expense/AddExpense';
 import ExpenseForm from '../../components/expense/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
-let wrapper, addExpense, history;
+let wrapper, startAddExpense, history;
 beforeEach(() => {
-  addExpense = jest.fn();
+  startAddExpense = jest.fn();
   history = { push: jest.fn() };
-  wrapper = shallow(<AddExpense addExpense={addExpense} history={history} />)
+  wrapper = shallow(<AddExpense startAddExpense={startAddExpense} history={history} />)
 });
 
 describe('Add Expense', () => {
@@ -19,7 +19,7 @@ describe('Add Expense', () => {
   it('should handle addExpense', () => {
     wrapper.find(ExpenseForm).prop('onSubmit')(expenses[1]);
     expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(addExpense).toHaveBeenLastCalledWith(expenses[1])
+    expect(startAddExpense).toHaveBeenLastCalledWith(expenses[1])
   })
 
 })
