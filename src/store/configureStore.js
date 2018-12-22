5fd30ddba09d65d5d8633b3bfe4ad6aa0b9995ea
cptiwari20 <cptiwari20@gmail.com>
2018-12-22 21:36:27 +0530

    auth reducers added to the redux store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import ReduxThunk from "redux-thunk";
 import expenseReducer from '../reducers/expenses';
 import filterReducer from '../reducers/filters';
+import authReducers from '../reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -9,7 +10,8 @@ export default () => {
   const store = createStore(
     combineReducers ({
       expenses: expenseReducer,
-      filters: filterReducer 
+      filters: filterReducer,
+      auth: authReducers
     }), 
     composeEnhancers(applyMiddleware(ReduxThunk))
   );
