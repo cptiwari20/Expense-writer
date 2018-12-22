d0e5dfe96a574cbf9455b51908a6f524b13630d6
cptiwari20 <cptiwari20@gmail.com>
2018-12-22 20:14:34 +0530

    added test for new button

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -3,9 +3,21 @@ import { shallow } from 'enzyme';
 // import  toJson from "enzyme-to-json";
 import { Header } from "../../components/Header";
 
+
+let wrapper, startSignOut;
+beforeEach(()=> {
+  startSignOut = jest.fn();
+  wrapper = shallow(<Header startSignOut={startSignOut} />);
+})
+
 it('should render Header correctly', () => {
-  const wrapper = shallow(<Header startSignOut={() => {}} />);
   expect(wrapper).toMatchSnapshot()
+})
+
+it('should call signOut Action on Button Click', () => {
+  wrapper.find('button').simulate('click');
+
+  expect(startSignOut).toBeCalled();
 })
 
 // import ReactShallowRenderer from "react-test-renderer/shallow";
