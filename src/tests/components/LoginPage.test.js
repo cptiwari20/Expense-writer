7d339380f4c3c373f18fead9cc1e3dd558757f2b
cptiwari20 <cptiwari20@gmail.com>
2018-12-22 20:20:39 +0530

    added test for the sign in button

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import loginPage, { LoginPage } from '../../components/LoginPage';
+import { LoginPage } from '../../components/LoginPage';
 
+let wrapper, startSignIn;
+beforeEach(() => {
+  startSignIn = jest.fn()
+  wrapper = shallow(<LoginPage startSignIn={startSignIn}/>)
+})
 describe('Login Page', () => {
   it('should render a loginPage Component', () => {
-    const wrapper = shallow(<LoginPage />)
     expect(wrapper).toMatchSnapshot()
+  });
+
+  it('should call signIn action on button click', () => {
+    wrapper.find('button').simulate('click');
+
+    expect(startSignIn).toBeCalled();
   })
 });
