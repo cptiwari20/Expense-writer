dfae4f7eb4943a84ec45af7ee0ab1f5ec4ec6f0a
cptiwari20 <cptiwari20@gmail.com>
2018-12-22 19:20:01 +0530

    added the startauth AC

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { startAuth } from '../actions/auth';
 
-export const LoginPage = () => (
+export const LoginPage = (props) => (
   <div>
-    <button> Login With Google </button>
+    <button onClick={() => {props.startAuth}}> Login With Google </button>
   </div>
-)
+);
+
+export default connect(null, { startAuth })(LoginPage);
