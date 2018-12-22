6e0910c1ea8154c63fbb0e0d30dd45598abf4f16
cptiwari20 <cptiwari20@gmail.com>
2018-12-22 21:33:16 +0530

    added login and logout ac

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,8 +1,21 @@
 import { firebase, GoogleAuthProvider } from '../firebase/firebase';
 
+export const login = (uid) => {
+  return {
+    type: "LOGIN",
+    payload: uid
+  }
+}
+
 export const startSignIn = () => () => {
   return firebase.auth().signInWithPopup(GoogleAuthProvider);
 };
+
+export const logout = () => {
+  return {
+    type: "LOGOUT"
+  }
+}
 
 export const startSignOut = () => () => {
   return firebase.auth().signOut();
