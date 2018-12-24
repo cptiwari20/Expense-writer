a5bb640fdbfe6dfb0aada3ba12f2bf7faee97cf7
cptiwari20 <cptiwari20@gmail.com>
2018-12-24 17:26:22 +0530

    added styled login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,11 @@ export const LoginPage = (props) => (
     <div className="box-layout__box">
       <h1 className="box-layout__title">Expense Writer</h1>
       <p>Write and manage your expenses anytime and anywhere.</p>
-      <button onClick={props.startSignIn}> Login With Google </button>
+      <button 
+        className='button'
+        onClick={props.startSignIn}> 
+        Login With Google 
+      </button>
     </div>
   </div>
 );
