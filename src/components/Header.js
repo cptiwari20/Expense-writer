d53401060534499d1cfdefacd8005c0e37b580fd
cptiwari20 <cptiwari20@gmail.com>
2018-12-24 17:58:15 +0530

    added header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,30 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startSignOut } from '../actions/auth';
 
 export const Header = (props) => (
-  <nav>
-    <h1>Expense Writer</h1>
-    <ul>
-      <li>
-        <NavLink to='/'  activeStyle={{
-          fontWeight: "bold",
-          color: "red"
-        }}
-        exact={true}>
-        Dashboard
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to='/add-expense'  activeStyle={{
-          fontWeight: "bold",
-          color: "red"
-        }}>
-        Create
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to='/about'  activeStyle={{
-          fontWeight: "bold",
-          color: "red"
-        }}>
-        About
-        </NavLink>
-      </li>
-      <li>
-        <button onClick={props.startSignOut}>Log Out</button>
-      </li>
-    </ul>
-  </nav>
+  <header className="header">
+    <div className='content-container'>
+      <div className='header__container'>
+        <Link to='/' 
+          className="header__title"
+          exact={true}>
+            <h1>Expense Writer</h1>
+        </Link>
+       <div>
+        <Link to='/about' className='header__link'>
+          About 
+        </Link>
+        <button 
+          className='button'
+          onClick={props.startSignOut}>
+          Log Out
+        </button>
+       </div>
+      </div>
+    </div>
+  </header>
 );
 
 export default connect(null, { startSignOut })(Header);
