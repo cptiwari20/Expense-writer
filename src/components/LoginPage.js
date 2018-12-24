ae8b37aab976cb108fe0a2266f129c9e9522cc59
cptiwari20 <cptiwari20@gmail.com>
2018-12-24 16:08:58 +0530

    started styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startSignIn } from '../actions/auth';
 
 export const LoginPage = (props) => (
-  <div>
-    <button onClick={props.startSignIn}> Login With Google </button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expense Writer</h1>
+      <p>Write and manage your expenses anytime and anywhere.</p>
+      <button onClick={props.startSignIn}> Login With Google </button>
+    </div>
   </div>
 );
 
